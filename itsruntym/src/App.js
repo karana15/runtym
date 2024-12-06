bb8afde643d2ac7ff2e38b707e8f827662e1138d
Ashish Singh <as4797singh@gmail.com>
2024-12-06 22:31:04 +0530

    RouteAdded

--- itsruntym/src/App.js
+++ itsruntym/src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
-import DashBoard from "./components/DashBoard"
+import React from 'react';
+import {Routes, Route, BrowserRouter} from "react-router-dom";
+import DSA from './components/Pages/DSA';
+import CSE from './components/Pages/CSE';
+import Java from './components/Pages/Java';
+import Home from './components/Pages/Home';
 
-function App() {
+export default function App() {
   return (
-    <DashBoard>
-      
-    </DashBoard>
-  );
+    <>
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" exact element={<Home/>}></Route>
+      <Route path="/DSA" exact element={<DSA/>}></Route>
+      <Route path="/CSE" exact element={<CSE/>}></Route>
+      <Route path="/Java" exact element={<Java/>}></Route>
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
 }
 
-export default App;
